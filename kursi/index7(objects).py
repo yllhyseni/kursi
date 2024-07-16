e29541587a1ffb497cd8f8667dff195dc8e9f9b8
yllhyseni <47318986+yllhyseni@users.noreply.github.com>
2024-07-16 11:34:36 +0200

    update 4
    added object

---
+++ kursi/index7(objects).py
@@ -0,0 +1,29 @@
+#person = {
+ #   "emri": "yll",
+ #   "mbiemri": "hyseni",
+ #   "shkolla": "don bosko",
+ #   "tituli": "student",
+ #   "mosha": "16"
+#}
+
+
+#emri = person.get("emri")
+# OR
+# emri = person["emri"]
+
+#print(emri)
+
+#erson["mosha"] = "15"
+
+#print(person)
+
+#for i in person:
+#    print(person[i])
+# OR
+#for i in person.values():
+ #   print(i)
+ 
+#if "kompania" in person:
+   # print("po eshte ne objekt")
+#else:
+  #  print("jo nuk eshte ne objekt")
