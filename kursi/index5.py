c2991bcf05ea1056d0994902b479546e827112f4
yllhyseni <47318986+yllhyseni@users.noreply.github.com>
2024-07-16 10:32:05 +0200

    version 2
    added method min and max (behind the seens)

--- kursi/index5.py
+++ kursi/index5.py
@@ -20,3 +20,19 @@ print(f"shuma e pagave eshte: {shuma_e_pagave}")
 
 paga_mesatare = shuma_e_pagave / len(salery)
 print(f"paga mesatare eshte: {paga_mesatare}")
+
+#paga_min = min(salery)
+#paga_max = max(salery)
+#print(f"paga minimale eshte: {paga_min}")
+#rint(f"paga maksimale eshte: {paga_max}")
+pagaMin = salery[0]
+for pagaAktuale in salery:
+    if pagaAktuale < pagaMin:
+        pagaMin = pagaAktuale
+print(f"paga minimale eshte: {pagaMin}")
+    
+pagamax = salery[0]
+for pagaAktuale in salery:
+    if pagaAktuale > pagamax:
+        pagamax = pagaAktuale
+print(f"paga maksimale eshte: {pagamax}")
