17bf4d83167f4da39d5d28a12a0a677bd45306b1
yllhyseni <47318986+yllhyseni@users.noreply.github.com>
2024-07-22 10:34:45 +0200

    update
    added instagram.py

---
+++ kursi/grupi veror yll/instagram.py
@@ -0,0 +1,8 @@
+import instaloader
+insta=instaloader.Instaloader()
+username=input("Enter the username:")
+
+profile=instaloader.Profile.from_username(insta.context,username)
+print("Username:",profile.username)
+print("Number of posts:",profile.mediacount)
+print("Followers",profile.followers)
